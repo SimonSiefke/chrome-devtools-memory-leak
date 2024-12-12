b7ec6340c8695f7791800b117f94d4f7caa15355
Simon Siefke <simon.siefke@gmail.com>
2024-12-12 22:47:44 +0100

    install playwright

--- packages/shared/index.js
+++ packages/shared/index.js
@@ -1,3 +1,5 @@
+import CDP from "chrome-remote-interface";
+
 const launchBrowser = async (headless, remoteDebuggingPort, playwrightPath) => {
   const { chromium } = await import(playwrightPath);
   const browser = await chromium.launch({
@@ -14,9 +16,30 @@ const launchBrowser = async (headless, remoteDebuggingPort, playwrightPath) => {
   };
 };
 
+const runDevtoolsCommands = async (commandCount, debuggingPort) => {
+  const client = await CDP({
+    host: "localhost",
+    port: Number(debuggingPort),
+  });
+  const { promise, resolve } = Promise.withResolvers();
+  client.Target.attachedToTarget(resolve);
+  await client.Target.setAutoAttach({
+    autoAttach: true,
+    waitForDebuggerOnStart: true,
+    flatten: true,
+  });
+  // @ts-ignore
+  const { sessionId } = await promise;
+  await client.Runtime.enable(sessionId);
+  const mem = await client.Runtime.getHeapUsage(sessionId);
+
+  console.log({ mem });
+};
+
 export const main = async (playwrightPath) => {
   const remoteDebuggingPort = "9222";
-  const headless = true;
+  const headless = process.argv.includes("--headless");
+  const commandCount = 100_000;
   const { page, browser } = await launchBrowser(
     headless,
     remoteDebuggingPort,
@@ -24,5 +47,6 @@ export const main = async (playwrightPath) => {
   );
   await page.goto("https://example.com");
   await page.waitForLoadState("networkidle");
+  await runDevtoolsCommands(commandCount, remoteDebuggingPort);
   await browser.close();
 };
