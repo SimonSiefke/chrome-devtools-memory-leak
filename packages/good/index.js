2d17833c9801000f2daa1726ea7ce7734ddf0fd4
Simon Siefke <simon.siefke@gmail.com>
2024-12-12 22:43:46 +0100

    add playwright

--- packages/good/index.js
+++ packages/good/index.js
@@ -1,1 +1,8 @@
-import "../shared/index.js";
+import { main } from "../shared/index.js";
+
+const playwrightPath = new URL(
+  "./node_modules/playwright/index.mjs",
+  import.meta.url
+).toString();
+
+main(playwrightPath);
--- packages/shared/index.js
+++ packages/shared/index.js
@@ -1,1 +1,28 @@
-console.log("hello");
+const launchBrowser = async (headless, remoteDebuggingPort, playwrightPath) => {
+  const { chromium } = await import(playwrightPath);
+  const browser = await chromium.launch({
+    headless: headless,
+    args: [`--remote-debugging-port=${remoteDebuggingPort}`],
+  });
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  return {
+    page,
+    browser,
+  };
+};
+
+export const main = async (playwrightPath) => {
+  const remoteDebuggingPort = "9222";
+  const headless = true;
+  const { page, browser } = await launchBrowser(
+    headless,
+    remoteDebuggingPort,
+    playwrightPath
+  );
+  await page.goto("https://example.com");
+  await page.waitForLoadState("networkidle");
+  await browser.close();
+};
